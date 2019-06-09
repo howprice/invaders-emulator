4f310167450bdebc57213f7160ac2f4eaa887c61
howprice <howardjohnprice@gmail.com>
2019-06-09 15:49:09 +0100

    Skeleton 'disassemble' project

---
+++ README.md
@@ -0,0 +1,11 @@
+# invaders-emulator
+
+---
+
+## About
+
+Space Invaders emulator. My first emulator. Inspired by the excellent tutorial at [Emulator 101](http://emulator101.com)
+
+## Links
+
+http://www.emutalk.net/threads/38177-Space-Invaders
---
+++ scripts/genie.lua
@@ -0,0 +1,63 @@
+solution "invaders-emulator"
+	location "../build"
+	configurations { "Debug", "Dev", "Release" }
+	platforms { "native", "x32", "x64" }
+
+	project "disassemble"
+		location "../build"
+		kind "ConsoleApp"
+		language "C++"
+		files {
+			"../src**"
+		}
+		includedirs {
+			"../src/core"
+		}
+		flags { "ExtraWarnings", "FatalWarnings" }
+		debugdir "../data"		-- debugger working directory
+		
+		configuration "Debug"
+			--defines { "_DEBUG" } Removed this Windows define because using /MD rather than /MDd
+			defines { "DEBUG" }
+			flags { "Symbols" }
+			targetdir "../bin/debug"
+
+		configuration "Dev"
+			flags { "Optimize", "Symbols" }			
+			targetdir "../bin/dev"
+	
+		configuration "Release"
+			defines { "NDEBUG", "RELEASE" }
+			flags { "Optimize" }
+			targetdir "../bin/release"
+			
+		configuration "windows"
+			flags { "ReleaseRuntime" }  
+			
+			defines { "_CRT_SECURE_NO_WARNINGS" }
+			
+			-- Disable compiler warnings. These end up in the Project Settings -> C/C++ -> Command Line -> Additional Options, rather than C/C++ -> Advanced -> Disable Specific Warnings 
+			buildoptions { "/wd4127" } -- conditional expression is constant
+			buildoptions { "/wd4505" } -- unreferenced local function has been removed
+			
+		configuration "linux"
+			buildoptions { "-std=c++0x" }
+			buildoptions { "-Wno-switch" }
+			buildoptions { "-Wno-unused-function" }
+			buildoptions { "-Wno-missing-field-initializers" }
+			buildoptions { "-Wno-missing-braces" }
+								
+newaction
+{
+	trigger = "clean",
+	shortname = "clean",
+	description = "Removes generated files."
+}
+			
+if _ACTION == "clean" then
+	os.rmdir("../bin")
+	if os.get() == "windows" then
+		os.outputof("rmdir ..\\build\\.vs /s /q") -- remove the hidden .vs directory
+	end
+	os.rmdir("../build")			-- this doesn't work because the directory contains .vs folder
+end
