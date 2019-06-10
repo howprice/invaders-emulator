3c769c83ddcd5835f4d0fafc21095896d2441e2f
howprice <howardjohnprice@gmail.com>
2019-06-10 07:52:19 +0300

    Show immediate data in disassembly

--- src/main.cpp
+++ src/main.cpp
@@ -15,12 +15,12 @@ struct Instruction
 static const Instruction s_instructions[] =
 {
 	{ 0x00, "NOP", 1 },
-	{ 0x01, "LXI B,D16", 3 },
+	{ 0x01, "LXI B,%04X", 3 },
 	{ 0x02, "STAX B", 1 }, //		(BC) < -A
 	{ 0x03, "INX B", 1 }, //		BC < -BC + 1
 	{ 0x04, "INR B", 1 }, //	Z, S, P, AC	B < -B + 1
 	{ 0x05, "DCR B", 1 }, //	Z, S, P, AC	B < -B - 1
-	{ 0x06, "MVI B, D8", 2 }, //		B < -byte 2
+	{ 0x06, "MVI B, %02X", 2 }, //		B < -byte 2
 	{ 0x07, "RLC",	1 }, //		CY	A = A << 1; bit 0 = prev bit 7; CY = prev bit 7
 	{ 0x08, "-", 0 },
 	{ 0x09, "DAD B",	1 }, //		CY	HL = HL + BC
@@ -28,15 +28,15 @@ static const Instruction s_instructions[] =
 	{ 0x0b, "DCX B",	1 }, //			BC = BC - 1
 	{ 0x0c, "INR C",	1 }, //		Z, S, P, AC	C < -C + 1
 	{ 0x0d, "DCR C",	1 }, //		Z, S, P, AC	C < -C - 1
-	{ 0x0e, "MVI C,D8",	2 }, //			C < -byte 2
+	{ 0x0e, "MVI C,%02X",	2 }, //			C < -byte 2
 	{ 0x0f, "RRC",	1 }, //		CY	A = A >> 1; bit 7 = prev bit 0; CY = prev bit 0
 	{ 0x10, "-", 0 }, //	
-	{ 0x11, "LXI D,D16",	3 }, //			D < -byte 3, E < -byte 2
+	{ 0x11, "LXI D,%04X",	3 }, //			D < -byte 3, E < -byte 2
 	{ 0x12, "STAX D",	1 }, //			(DE) < -A
 	{ 0x13, "INX D",	1 }, //			DE < -DE + 1
 	{ 0x14, "INR D",	1 }, //		Z, S, P, AC	D < -D + 1
 	{ 0x15, "DCR D",	1 }, //		Z, S, P, AC	D < -D - 1
-	{ 0x16, "MVI D, D8",	2 }, //			D < -byte 2
+	{ 0x16, "MVI D, %02X",	2 }, //			D < -byte 2
 	{ 0x17, "RAL",	1 }, //		CY	A = A << 1; bit 0 = prev CY; CY = prev bit 7
 	{ 0x18, "-", 0 }, //	
 	{ 0x19, "DAD D",	1 }, //		CY	HL = HL + DE
@@ -44,39 +44,39 @@ static const Instruction s_instructions[] =
 	{ 0x1b, "DCX D",	1 }, //			DE = DE - 1
 	{ 0x1c, "INR E",	1 }, //		Z, S, P, AC	E < -E + 1
 	{ 0x1d, "DCR E",	1 }, //		Z, S, P, AC	E < -E - 1
-	{ 0x1e, "MVI E,D8",	2 }, //			E < -byte 2
+	{ 0x1e, "MVI E,%02X",	2 }, //			E < -byte 2
 	{ 0x1f, "RAR",	1 }, //		CY	A = A >> 1; bit 7 = prev bit 7; CY = prev bit 0
 	{ 0x20, "-", 0 }, //	
-	{ 0x21, "LXI H,D16",	3 }, //			H < -byte 3, L < -byte 2
-	{ 0x22, "SHLD adr",	3 }, //			(adr) < -L; (adr + 1) < -H
+	{ 0x21, "LXI H,%04X",	3 }, //			H < -byte 3, L < -byte 2
+	{ 0x22, "SHLD %04X",	3 }, //			(adr) < -L; (adr + 1) < -H
 	{ 0x23, "INX H",	1 }, //			HL < -HL + 1
 	{ 0x24, "INR H",	1 }, //		Z, S, P, AC	H < -H + 1
 	{ 0x25, "DCR H",	1 }, //		Z, S, P, AC	H < -H - 1
-	{ 0x26, "MVI H,D8",	2 }, //			H < -byte 2
+	{ 0x26, "MVI H,%02X",	2 }, //			H < -byte 2
 	{ 0x27, "DAA",	1 }, //			special
 	{ 0x28, "-", 0 }, //	
 	{ 0x29, "DAD H",	1 }, //		CY	HL = HL + HI
-	{ 0x2a, "LHLD adr",	3 }, //			L < -(adr); H < -(adr + 1)
+	{ 0x2a, "LHLD %04X",	3 }, //			L < -(adr); H < -(adr + 1)
 	{ 0x2b, "DCX H", 1 }, //			HL = HL - 1
 	{ 0x2c, "INR L", 1 }, //		Z, S, P, AC	L < -L + 1
 	{ 0x2d, "DCR L", 1 }, //		Z, S, P, AC	L < -L - 1
-	{ 0x2e, "MVI L, D8", 2 }, //			L < -byte 2
+	{ 0x2e, "MVI L, %02X", 2 }, //			L < -byte 2
 	{ 0x2f, "CMA", 1 }, //			A < -!A
 	{ 0x30, "-", 0 }, //	
-	{ 0x31, "LXI SP, D16",	3 }, //			SP.hi < -byte 3, SP.lo < -byte 2
-	{ 0x32, "STA adr",	3 }, //			(adr) < -A
+	{ 0x31, "LXI SP, %04X",	3 }, //			SP.hi < -byte 3, SP.lo < -byte 2
+	{ 0x32, "STA %04X",	3 }, //			(adr) < -A
 	{ 0x33, "INX SP",	1 }, //			SP = SP + 1
 	{ 0x34, "INR M",	1 }, //		Z, S, P, AC(HL) < -(HL)+1
 	{ 0x35, "DCR M",	1 }, //		Z, S, P, AC(HL) < -(HL)-1
-	{ 0x36, "MVI M,D8",	2 }, //			(HL) < -byte 2
+	{ 0x36, "MVI M,%02X",	2 }, //			(HL) < -byte 2
 	{ 0x37, "STC",	1 }, //		CY	CY = 1
 	{ 0x38, "-", 0},
 	{ 0x39, "DAD SP",	1 }, //		CY	HL = HL + SP
-	{ 0x3a, "LDA adr",	3 }, //			A < -(adr)
+	{ 0x3a, "LDA %04X",	3 }, //			A < -(adr)
 	{ 0x3b, "DCX SP",	1 }, //			SP = SP - 1
 	{ 0x3c, "INR A",	1 }, //		Z, S, P, AC	A < -A + 1
 	{ 0x3d, "DCR A",	1 }, //		Z, S, P, AC	A < -A - 1
-	{ 0x3e, "MVI A,D8",	2 }, //			A < -byte 2
+	{ 0x3e, "MVI A,%02X",	2 }, //			A < -byte 2
 	{ 0x3f, "CMC",	1 }, //		CY	CY = !CY
 	{ 0x40, "MOV B,B", 1 }, //			B < -B
 	{ 0x41, "MOV B,C", 1 }, //			B < -C
@@ -208,67 +208,67 @@ static const Instruction s_instructions[] =
 	{ 0xbf, "CMP A", 1 }, //		Z, S, P, CY, AC	A - A
 	{ 0xc0, "RNZ", 1 }, //			if NZ, RET
 	{ 0xc1, "POP B", 1 }, //			C < -(sp); B < -(sp + 1); sp < -sp + 2
-	{ 0xc2, "JNZ adr", 3 }, //			if NZ, PC < -adr
-	{ 0xc3, "JMP adr", 3 }, //			PC <= adr
-	{ 0xc4, "CNZ adr", 3 }, //			if NZ, CALL adr
+	{ 0xc2, "JNZ %04X", 3 }, //			if NZ, PC < -adr
+	{ 0xc3, "JMP %04X", 3 }, //			PC <= adr
+	{ 0xc4, "CNZ %04X", 3 }, //			if NZ, CALL adr
 	{ 0xc5, "PUSH B", 1 }, //			(sp - 2) < -C; (sp - 1) < -B; sp < -sp - 2
-	{ 0xc6, "ADI D8", 2 }, //		Z, S, P, CY, AC	A < -A + byte
+	{ 0xc6, "ADI %02X", 2 }, //		Z, S, P, CY, AC	A < -A + byte
 	{ 0xc7, "RST 0",	1 }, //			CALL $0
 	{ 0xc8, "RZ	1",	1 }, //		if Z, RET
 	{ 0xc9, "RET",	1 }, //			PC.lo < -(sp); PC.hi < -(sp + 1); SP < -SP + 2
-	{ 0xca, "JZ adr", 3 }, //			if Z, PC < -adr
+	{ 0xca, "JZ %04X", 3 }, //			if Z, PC < -adr
 	{ 0xcb, "-", 0 }, //	
-	{ 0xcc, "CZ adr",	3 }, //			if Z, CALL adr
-	{ 0xcd, "CALL adr",	3 }, //			(SP - 1) < -PC.hi; (SP - 2) < -PC.lo; SP < -SP - 2; PC = adr
-	{ 0xce, "ACI D8",	2 }, //		Z, S, P, CY, AC	A < -A + data + CY
+	{ 0xcc, "CZ %04X",	3 }, //			if Z, CALL adr
+	{ 0xcd, "CALL %04X",	3 }, //			(SP - 1) < -PC.hi; (SP - 2) < -PC.lo; SP < -SP - 2; PC = adr
+	{ 0xce, "ACI %02X",	2 }, //		Z, S, P, CY, AC	A < -A + data + CY
 	{ 0xcf, "RST 1",	1 }, //			CALL $8
 	{ 0xd0, "RNC",	1 }, //			if NCY, RET
 	{ 0xd1, "POP D",	1 }, //			E < -(sp); D < -(sp + 1); sp < -sp + 2
-	{ 0xd2, "JNC adr", 3 }, //			if NCY, PC < -adr
-	{ 0xd3, "OUT D8",	2 }, //			special
-	{ 0xd4, "CNC adr",	3 }, //			if NCY, CALL adr
+	{ 0xd2, "JNC %04X", 3 }, //			if NCY, PC < -adr
+	{ 0xd3, "OUT %02X",	2 }, //			special
+	{ 0xd4, "CNC %04X",	3 }, //			if NCY, CALL adr
 	{ 0xd5, "PUSH D",	1 }, //			(sp - 2) < -E; (sp - 1) < -D; sp < -sp - 2
-	{ 0xd6, "SUI D8", 2 }, //		Z, S, P, CY, AC	A < -A - data
+	{ 0xd6, "SUI %02X", 2 }, //		Z, S, P, CY, AC	A < -A - data
 	{ 0xd7, "RST 2",	1 }, //			CALL $10
 	{ 0xd8, "RC", 1 }, //			if CY, RET
 	{ 0xd9, "-", 0 }, //	
-	{ 0xda, "JC adr",	3 }, //			if CY, PC < -adr
-	{ 0xdb, "IN D8",	2 }, //			special
-	{ 0xdc, "CC adr",	3 }, //			if CY, CALL adr
+	{ 0xda, "JC %04X",	3 }, //			if CY, PC < -adr
+	{ 0xdb, "IN %02X",	2 }, //			special
+	{ 0xdc, "CC %04X",	3 }, //			if CY, CALL adr
 	{ 0xdd, "-", 0 }, //	
-	{ 0xde, "SBI D8",	2 }, //		Z, S, P, CY, AC	A < -A - data - CY
+	{ 0xde, "SBI %02X",	2 }, //		Z, S, P, CY, AC	A < -A - data - CY
 	{ 0xdf, "RST 3",	1 }, //			CALL $18
 	{ 0xe0, "RPO",	1 }, //			if PO, RET
 	{ 0xe1, "POP H",	1 }, //			L < -(sp); H < -(sp + 1); sp < -sp + 2
-	{ 0xe2, "JPO adr", 3 }, //			if PO, PC < -adr
+	{ 0xe2, "JPO %04X", 3 }, //			if PO, PC < -adr
 	{ 0xe3, "XTHL",	1 }, //			L < -> (SP); H < -> (SP + 1)
-	{ 0xe4, "CPO adr", 3 }, //			if PO, CALL adr
+	{ 0xe4, "CPO %04X", 3 }, //			if PO, CALL adr
 	{ 0xe5, "PUSH H",	1 }, //			(sp - 2) < -L; (sp - 1) < -H; sp < -sp - 2
-	{ 0xe6, "ANI D8", 2 }, //		Z, S, P, CY, AC	A < -A & data
+	{ 0xe6, "ANI %02X", 2 }, //		Z, S, P, CY, AC	A < -A & data
 	{ 0xe7, "RST 4",	1 }, //			CALL $20
 	{ 0xe8, "RPE",	1 }, //			if PE, RET
 	{ 0xe9, "PCHL",	1 }, //			PC.hi < -H; PC.lo < -L
-	{ 0xea, "JPE adr",	3 }, //			if PE, PC < -adr
+	{ 0xea, "JPE %04X",	3 }, //			if PE, PC < -adr
 	{ 0xeb, "XCHG",	1 }, //			H < ->D; L < ->E
-	{ 0xec, "CPE adr", 3 }, //			if PE, CALL adr
+	{ 0xec, "CPE %04X", 3 }, //			if PE, CALL adr
 	{ 0xed, "-", 0 }, //	
-	{ 0xee, "XRI D8",	2 }, //		Z, S, P, CY, AC	A < -A ^ data
+	{ 0xee, "XRI %02X",	2 }, //		Z, S, P, CY, AC	A < -A ^ data
 	{ 0xef, "RST 5",	1 }, //			CALL $28
 	{ 0xf0, "RP", 1 }, //			if P, RET
 	{ 0xf1, "POP PSW",	1 }, //			flags < -(sp); A < -(sp + 1); sp < -sp + 2
-	{ 0xf2, "JP adr", 3 }, //			if P = 1 PC < -adr
+	{ 0xf2, "JP %04X", 3 }, //			if P = 1 PC < -adr
 	{ 0xf3, "DI",	1 }, //			special
-	{ 0xf4, "CP adr",	3 }, //			if P, PC < -adr
+	{ 0xf4, "CP %04X",	3 }, //			if P, PC < -adr
 	{ 0xf5, "PUSH PSW",	1 }, //			(sp - 2) < -flags; (sp - 1) < -A; sp < -sp - 2
-	{ 0xf6, "ORI D8", 2 }, //		Z, S, P, CY, AC	A < -A | data
+	{ 0xf6, "ORI %02X", 2 }, //		Z, S, P, CY, AC	A < -A | data
 	{ 0xf7, "RST 6",	1 }, //			CALL $30
 	{ 0xf8, "RM",	1 }, //			if M, RET
 	{ 0xf9, "SPHL",	1 }, //			SP = HL
-	{ 0xfa, "JM adr",	3 }, //			if M, PC < -adr
+	{ 0xfa, "JM %04X",	3 }, //			if M, PC < -adr
 	{ 0xfb, "EI",	1 }, //			special
-	{ 0xfc, "CM adr",	3 }, //			if M, CALL adr
+	{ 0xfc, "CM %04X",	3 }, //			if M, CALL adr
 	{ 0xfd, "-", 0 }, //	
-	{ 0xfe, "CPI D8",	2 }, //		Z, S, P, CY, AC	A - data
+	{ 0xfe, "CPI %02X",	2 }, //		Z, S, P, CY, AC	A - data
 	{ 0xff, "RST 7",	1 }, //			CALL $38
 };
 static_assert(COUNTOF_ARRAY(s_instructions) == 256, "Array size incorrect");
@@ -296,8 +296,22 @@ static unsigned int Disassemble8080(const uint8_t* buffer, const size_t bufferSi
 		printf("   ");
 
 	// mnemonic
-	// #TODO: print properly
-	printf(" %s\n", instruction.mnemonic);
+	if(instruction.size == 1)
+		printf(" %s\n", instruction.mnemonic);
+	else if(instruction.size == 2)
+	{
+		uint8_t d8 = *(pPC + 1);
+		char text[64];
+		sprintf(text, instruction.mnemonic, d8);
+		printf(" %s\n", text);
+	}
+	else if(instruction.size == 3)
+	{
+		uint16_t val = *(uint16_t*)(pPC + 1); // assume host is little endian
+		char text[64];
+		sprintf(text, instruction.mnemonic, val);
+		printf(" %s\n", text);
+	}
 	return instruction.size;
 }
 
