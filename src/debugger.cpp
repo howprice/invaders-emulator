701f2fe33881b6e7ba09c1c007d354ab398df8a9
howprice <howprice@users.noreply.github.com>
2019-06-15 08:05:11 +0300

    Implemented first few instructions required by invaders ROM

--- src/debugger.cpp
+++ src/debugger.cpp
@@ -6,12 +6,21 @@
 
 void PrintState(const State8080& state)
 {
+#if 0
 	printf("A: %02X  Flags: %02X\n", state.A, state.Flags);
 	printf("B: %02X  C: %02X\n", state.B, state.C);
 	printf("D: %02X  E: %02X\n", state.D, state.E);
 	printf("H: %02X  L: %02X\n", state.H, state.L);
 	printf("SP: %04X\n", state.SP);
 	printf("PC: %04X\n", state.PC);
-
+#else
+	// single line
+	printf("A: %02X  F: %02X  ", state.A, state.Flags);
+	printf("B: %02X  C: %02X  ", state.B, state.C);
+	printf("D: %02X  E: %02X  ", state.D, state.E);
+	printf("H: %02X  L: %02X  ", state.H, state.L);
+	printf("SP: %04X  ", state.SP);
+	printf("PC: %04X\n", state.PC);
+#endif
 	// #TODO: Print individual flags S Z - AC - P - C
 }
