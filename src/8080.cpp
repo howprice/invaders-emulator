4fa3dbc90e706839d7e1c5c8c29700255ffba8c8
howprice <howprice@users.noreply.github.com>
2019-06-16 07:56:24 +0100

    Implemented instruction 0xD5  PUSH D, where D is the register pair DE

--- src/8080.cpp
+++ src/8080.cpp
@@ -263,6 +263,20 @@ static void executeC9(State8080& state)
 	state.PC = address;
 }
 
+// 0xD5  PUSH D, where D is the register pair DE
+// (sp - 2) <- E; (sp - 1) <- D; sp <- sp-2
+static void executeD5(State8080& state)
+{
+	// First register of register pair always contains the MSB
+	// Store in memory in little-endian format.
+	uint8_t msb = state.D;
+	uint8_t lsb = state.E;
+	HP_ASSERT(state.SP >= 2);
+	writeByteToMemory(state, state.SP - 2, lsb);
+	writeByteToMemory(state, state.SP - 1, msb);
+	state.SP -= 2;
+}
+
 // 0xFE  CPI d8
 // Compare immediate with accumulator.
 // The comparison is performed by internally subtracting the data from the accumulator,
@@ -496,7 +510,7 @@ static const Instruction s_instructions[] =
 	{ 0xd2, "JNC %04X", 3, nullptr }, //			if NCY, PC < -adr
 	{ 0xd3, "OUT %02X",	2, nullptr }, //			special
 	{ 0xd4, "CNC %04X",	3, nullptr }, //			if NCY, CALL adr
-	{ 0xd5, "PUSH D",	1, nullptr }, //			(sp - 2) < -E; (sp - 1) < -D; sp < -sp - 2
+	{ 0xd5, "PUSH D",	1, executeD5 }, // (sp - 2) <- E; (sp - 1) <- D; sp <- sp-2
 	{ 0xd6, "SUI %02X", 2, nullptr }, //		Z, S, P, CY, AC	A < -A - data
 	{ 0xd7, "RST 2",	1, nullptr }, //			CALL $10
 	{ 0xd8, "RC", 1, nullptr }, //			if CY, RET
