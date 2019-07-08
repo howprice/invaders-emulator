9511629330cb57da3cc5ff6d4029333893566971
howprice <howardjohnprice@gmail.com>
2019-07-08 21:03:58 +0100

    Moved debug helper functions into debugger.cpp

--- src/8080.cpp
+++ src/8080.cpp
@@ -2827,7 +2827,9 @@ const char* GetInstructionMnemonic(uint8_t opcode)
 
 unsigned int GetInstructionSizeBytes(uint8_t opcode)
 {
-	return s_instructions[opcode].sizeBytes;
+	const Instruction& instruction = s_instructions[opcode];
+	HP_ASSERT(instruction.sizeBytes >= kMinInstructionSizeBytes && instruction.sizeBytes <= kMaxInstructionSizeBytes);
+	return instruction.sizeBytes;
 }
 
 // returns the number of cycles that the instruction took to execute
@@ -2887,38 +2889,4 @@ void Reset(State8080& state)
 {
 	state.PC = 0;
 	state.halt = 0;
-}
-
-uint16_t GetNextInstructionAddress(const State8080& state)
-{
-	const uint8_t opcode = readByteFromMemory(state, state.PC);
-	const Instruction& instruction = s_instructions[opcode];
-	HP_ASSERT(instruction.sizeBytes >= kMinInstructionSizeBytes && instruction.sizeBytes <= kMaxInstructionSizeBytes);
-	uint16_t nextInstructionAddress = state.PC + instruction.sizeBytes;
-	return nextInstructionAddress;
-}
-
-bool CurrentInstructionIsACall(const State8080& state)
-{
-	const uint8_t opcode = readByteFromMemory(state, state.PC);
-	if(opcode == 0xCD)
-		return true; // CALL
-	if(opcode == 0xDC)
-		return true; // CC
-	if(opcode == 0xD4)
-		return true; // CNC
-	if(opcode == 0xCC)
-		return true; // CZ
-	if(opcode == 0xC4)
-		return true; // CNZ
-	if(opcode == 0xFC)
-		return true; // CM
-	if(opcode == 0xF4)
-		return true; // CP
-	if(opcode == 0xEC)
-		return true; // CPE
-	if(opcode == 0xE4)
-		return true; // CPO
-	
-	return false;
 }
