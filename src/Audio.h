7c085d4ad19dd9ab35a7f21d33e9b6a7e8e15a2f
howprice <howprice@users.noreply.github.com>
2019-07-12 14:23:50 +0100

    Don't fail if audio files missing

--- src/Audio.h
+++ src/Audio.h
@@ -1,6 +1,6 @@
 #pragma once
 
-bool InitAudio();
-void ShutdownAudio();
+void InitGameAudio();
+void ShutdownGameAudio();
 void PlaySample(unsigned int index);
 void StopSample(unsigned int index);
