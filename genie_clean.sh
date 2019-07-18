1a75aba2f29398e88e10e4bca7eea9803dce1df5
howprice <howprice@users.noreply.github.com>
2019-07-18 17:58:35 +0100

    Mac OSX fixes

--- genie_clean.sh
+++ genie_clean.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
-    ./tools/bin/linux/genie gmake
+    ./tools/bin/linux/genie clean
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     # Mac OSX
-    ./tools/bin/macosx/genie gmake
+    ./tools/bin/macosx/genie clean
 fi
