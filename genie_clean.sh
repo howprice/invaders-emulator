91ed4241478fba1ec648a9a4429d2b9f4b1986e1
howprice <howprice@users.noreply.github.com>
2019-07-19 17:49:01 +0100

    Fixed shell scripts for Raspberry Pi

--- genie_clean.sh
+++ genie_clean.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
     ./tools/bin/linux/genie clean
+elif [[ "$OSTYPE" == "linux-gnueabihf" ]]; then
+    ./tools/bin/raspberry-pi/genie clean
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     # Mac OSX
     ./tools/bin/macosx/genie clean
