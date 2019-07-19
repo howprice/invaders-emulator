79f1bf0e740371871e526ec521e33c00bfb6d6e9
howprice <howardjohnprice@gmail.com>
2019-07-19 17:49:01 +0100

    Fixed shell scripts for Raspberry Pi

--- genie_gmake.sh
+++ genie_gmake.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
     ./tools/bin/linux/genie gmake
+elif [[ "$OSTYPE" == "linux-gnueabihf" ]]; then
+    ./tools/bin/raspberry-pi/genie gmake
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     # Mac OSX
     ./tools/bin/macosx/genie gmake
