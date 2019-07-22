e44caf3f2d052942cb40bae4f75298d4dac2a93f
howprice <howprice@users.noreply.github.com>
2019-07-22 19:34:47 +0100

    Display options

--- src/Display.h
+++ src/Display.h
@@ -6,6 +6,20 @@
 
 #include <GL/gl3w.h>
 
+enum class HorizontalAlignment
+{
+	Left,
+	Centre,
+	Right
+};
+
+enum class VerticalAlignment
+{
+	Bottom,
+	Centre,
+	Top,
+};
+
 class Display
 {
 public:
@@ -17,19 +31,36 @@ public:
 	void Render();
 	void Present();
 
-	void GetSize(unsigned int &width, unsigned int &height) const;
-	void SetZoom(unsigned int zoom);
+	void GetWindowClientSize(unsigned int &width, unsigned int &height) const;
+	void SetWindowScale(unsigned int scale);
+
 	void SetRotate(bool rotate);
 	bool GetRotate() const { return m_rotate; }
+
 	bool IsFullscreen() const;
 	void SetFullscreen(bool fullscreen);
 	void ToggleFullscreen();
-	bool GetBilinearSampling() const { return m_bilinearSampling; }
-	void SetBilinearSampling(bool bilinearSampling) { m_bilinearSampling = bilinearSampling; }
 
 	bool IsVsyncAvailable() const { return m_vsyncAvailable; }
 	bool IsVsyncEnabled() const { return m_vsyncEnabled; }
 	bool SetVsyncEnabled(bool enabled);
+
+	void SetImageScale(unsigned int scale);
+
+	bool GetFitWindow() const { return m_fitWindow; }
+	void SetFitWindow(bool val) { m_fitWindow = val; }
+
+	bool GetMaintainAspectRatio() const { return m_maintainAspectRatio; }
+	void SetMaintainAspectRatio(bool val) { m_maintainAspectRatio = val; }
+
+	HorizontalAlignment GetHorizontalAlignment() const { return m_horizontalAlignment; }
+	void SetHorizontalAlignment(HorizontalAlignment val) { m_horizontalAlignment = val; }
+
+	VerticalAlignment GetVerticalAlignment() const { return m_verticalAlignment; }
+	void SetVerticalAlignment(VerticalAlignment val) { m_verticalAlignment = val; }
+
+	bool GetBilinearSampling() const { return m_bilinearSampling; }
+	void SetBilinearSampling(bool bilinearSampling) { m_bilinearSampling = bilinearSampling; }
 
 	void SetByte(unsigned int address, uint8_t value);
 
@@ -38,18 +69,19 @@ public:
 
 private:
 
-	Display();
+	Display(unsigned int width, unsigned int height);
 	~Display();
 
-	bool init(const unsigned int width, const unsigned int height, unsigned int zoom, bool rotate, bool bFullscreen);
+	bool init(unsigned int zoom, bool rotate, bool bFullscreen);
 
 	void updateTexture();
 
 	SDL_Window* m_pWindow = nullptr;
 	SDL_GLContext m_GLContext = nullptr;
 
-	unsigned int m_width = 0;
-	unsigned int m_height = 0;
+	const unsigned int m_width = 0;  // Machine display width in pixels. Not window width. 
+	const unsigned int m_height = 0; // Machine display height in pixels. Not window height
+
 	uint8_t* m_pDisplayBuffer = nullptr; // w * h * 1 bit per pixel
 	unsigned int m_displayBufferSizeBytes = 0;
 	bool m_vsyncAvailable = false;
@@ -58,7 +90,14 @@ private:
 
 	uint8_t* m_pTexturePixelsR8 = nullptr; // 1 byte per pixel
 	GLuint m_texture = 0; // same dimensions as display
-	bool m_bilinearSampling = true;
+
+	// render params
+	unsigned int m_imageScale = 2;
+	bool m_fitWindow = true;
+	bool m_maintainAspectRatio = true;
+	HorizontalAlignment m_horizontalAlignment = HorizontalAlignment::Centre;
+	VerticalAlignment m_verticalAlignment = VerticalAlignment::Centre;
+	bool m_bilinearSampling = false;
 };
 
 #endif
