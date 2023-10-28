ad385a074f709390f89563bb85454f92607cc8ef
howprice <howprice@users.noreply.github.com>
2023-10-28 15:56:59 +0100

    Add genie_vs2022.bat
    Seems to build in fine VS2022 with no changes.
    Deleted old 2017 and 2019 scripts.

--- scripts/genie_vs2017.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-SET script_path=%~dp0
-pushd %script_path%
-..\tools\bin\windows\genie.exe vs2017
-popd
--- scripts/genie_vs2022.bat
+++ scripts/genie_vs2022.bat
@@ -1,5 +1,5 @@
 @echo off
 SET script_path=%~dp0
 pushd %script_path%
-..\tools\bin\windows\genie.exe vs2019
+..\tools\bin\windows\genie.exe vs2022
 popd
